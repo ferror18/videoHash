cfc8977b39b28a3bf77cdc92310afccdacb5c3d5
ferror18 <ferror18@gmail.com>
2022-03-23 08:44:28 -0700

    Hash single video

--- video_hash.py
+++ video_hash.py
@@ -1,1 +1,17 @@
-print('hello world')
+#!/usr/bin/python
+
+from ast import For
+import os, sys
+
+# Open a file
+path = "C:/Users/murad/Desktop/videoHash/videos"
+dirs = os.listdir( path )
+
+# This would print all the files and directories
+# for file in dirs:
+#    print(file)
+
+from videohash import VideoHash
+os.chdir('videos')
+vid =  VideoHash('videos/0bf362fa47231a9e523f7243bc081be2.mp4')
+print('Hash -->', vid.hash)
