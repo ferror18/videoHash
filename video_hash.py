780862982ebb935e603611432b391fb0eac90855
ferror18 <ferror18@gmail.com>
2022-03-23 09:22:45 -0700

    loop hash all vids

--- video_hash.py
+++ video_hash.py
@@ -7,11 +7,32 @@ import os, sys
 path = "C:/Users/murad/Desktop/videoHash/videos"
 dirs = os.listdir( path )
 
-# This would print all the files and directories
-# for file in dirs:
-#    print(file)
+
 
 from videohash import VideoHash
+
+lookup_table = {}
 os.chdir('videos')
-vid =  VideoHash('videos/0bf362fa47231a9e523f7243bc081be2.mp4')
-print('Hash -->', vid.hash)
+# This would print all the files and directories
+for file in dirs:
+    vid =  VideoHash(file, frame_interval=5)
+    if vid.hash in lookup_table:
+        print('Collision')
+        print('New: ', file, 'Old: ', lookup_table[vid.hash], 'hash: ', vid.hash)
+        print('________________________________________')
+        pass
+    else:
+        lookup_table[vid.hash] = file
+        print('Name:',file,'  Hash -->', vid.hash)
+
+
+def test2Vid(vid1, vid2):
+    os.chdir('videos')
+    print('Running Test')
+    hash1 = VideoHash(vid1, frame_interval=5).hash
+    hash2 = VideoHash(vid2, frame_interval=5).hash
+    if hash1 == hash2:
+        print('Failed')
+    else:
+        print('Passed')
+# test2Vid('18.mp4', '22.mp4')
