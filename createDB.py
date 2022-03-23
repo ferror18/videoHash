ec10a48b5977fabcced5e9342bc8f22095e16329
ferror18 <ferror18@gmail.com>
2022-03-23 10:27:13 -0700

    Create database script

--- createDB.py
+++ createDB.py
@@ -3,19 +3,30 @@ import sqlite3
 import os
 
 # Open a file
-path = "C:/Users/murad/Desktop/videoHash/videos"
+path = "C:/Users/murad/Desktop/videoHash"
 dirs = os.listdir( path )
 db_exists = False
 
 for file in dirs:
-    if file == 'vhdb.db':
+    # print(file)
+    if file == 'vh.db':
         db_exists = True
     else:
         pass
 
-if db_exists:
-    print(db_exists)
-    open('vhdb.db','x')
-        
-con = sqlite3.connect('vhdb.db')
-cur = con.cursor()
+if not db_exists:
+    print('New database was created with the name vh.db')
+    open('vh.db','x')        
+    con = sqlite3.connect('vh.db')
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE Videos( hash PRIMARY KEY, name UNIQUE, path UNIQUE)''')
+    con.commit()
+    print('TABLE SCHEMA ==> Videos( hash PRIMARY KEY, name UNIQUE, path UNIQUE)')
+    con.close()
+
+
+
+
+
+else:
+    print('Db already exists')
