8b04d69ce842fe927797f2e911d23e99eba30553
ferror18 <ferror18@gmail.com>
2022-03-24 11:52:25 -0700

    Photo and video hash

--- modules/video_hash.py
+++ modules/video_hash.py
@@ -1,36 +1,4 @@
-#!/usr/bin/python
+from videohash import VideoHash as vh
 
-import os, sys
-
-# Open a file
-path = "C:/Users/murad/Desktop/videoHash/videos"
-dirs = os.listdir( path )
-
-
-
-from videohash import VideoHash
-
-lookup_table = {}
-os.chdir('videos')
-# This would print all the files and directories
-for file in dirs:
-    vid =  VideoHash(file, frame_interval=5)
-    if vid.hash_hex in lookup_table:
-        print('Collision')
-        print('New: ', file, 'Old: ', lookup_table[vid.hash_hex], 'hash: ', vid.hash_hex)
-        print('________________________________________')
-        pass
-    else:
-        lookup_table[vid.hash_hex] = file
-        print('Name:',file,'  Hash -->', vid.hash_hex)
-
-
-def test2Vid(vid1, vid2, fi=5):
-    print('Running Test')
-    hash1 = VideoHash(vid1, frame_interval=fi).hash_hex
-    hash2 = VideoHash(vid2, frame_interval=fi).hash_hex
-    if hash1 == hash2:
-        print('Failed')
-    else:
-        print('Passed')
-# test2Vid('18.mp4', '22.mp4')
+def video_hash(path, fi=5):
+    return vh(path, frame_interval=fi)
