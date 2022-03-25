2bc2171ebe9b6fb4506e608ef498c5d357ab0a9e
ferror18 <ferror18@gmail.com>
2022-03-24 20:27:04 -0700

    .

--- main.py
+++ main.py
@@ -6,23 +6,47 @@ from modules.photo_hash import photo_hash
 from modules.generate_config_file import generate_config_file 
 from modules.clear_console import clearConsole
 clearConsole()
+config = {}
 while True:
     print('''
     What would you like to do?
 
     a) Sort collection
-    b) Generate config file
-    c) exit
+    b) Review collisions
+    c) Generate config file
+    d) exit
     ''')
     inp = input("")
     clearConsole()
-    if inp == "c":
+    if inp == "d":
         break
-    elif inp == "b":
+    elif inp == "c":
         generate_config_file()
-    elif inp == "a":
-        # runTests()
+    elif inp == "b":
+        #Review collisions
         pass
+    elif inp == "a":
+        # run tests
+        # Config logic
+        conFile = open("config.txt", "r")
+        conFile = conFile.readlines()
+        conFile = [i for i in conFile if i != '\n' and i[0] != '#' ]
+        conFile = [i.replace('\n','') for i in conFile]
+        conFile = [i.split('=') for i in conFile]
+        for i in conFile:
+            config[i[0]]=i[1]
+        # pprint(config)
+
+        if config['mode'] == 'episodes_same_folder':
+            pass
+        elif config['mode'] == 'photos_oredered_by_month':
+            pass
+        else:
+            print('Unknown mode')
+        # Map files
+        # Get hashes
+        # Review collisions
+        # Move files
 
     else:
         continue
